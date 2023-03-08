82a61d3d814b1751e9b838da7787df333e379f2d
Anthony Delagdo <adelg180@fiu.edu>
2023-03-08 12:20:11 -0500

    Testing Mongol Script

---
+++ Main.py
@@ -0,0 +1,5 @@
+import Package as p
+
+#Testing
+father = "" + p.MongolScript["a"] + p.MongolScript["m"] + p.MongolScript["a"]
+print(father) #how you say father in manchu, currently won't print correctly
